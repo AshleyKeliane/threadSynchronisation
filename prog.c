138edd0446bc71d78278a604aed00ff58c7d1c28
AshleyKeliane <146139879+AshleyKeliane@users.noreply.github.com>
2025-03-06 21:28:46 -0500

    first commit

---
+++ prog.c
@@ -0,0 +1,60 @@
+/*   Noms et prenoms: Manjanjoh Tamanjong Ashley Keliane  */
+/*   NO etudiant: 300327351   */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main(){
+
+    int turn;
+    int a; // Number of full cycles
+    int b;
+
+    void* thread_main(void* arg) {
+        pthread_t threads[4];    // main thread creating 4 other threads
+        int thread_ids[4] = {0, 1, 2, 3};
+
+        for (int i = 0; i < 4; i++) {
+            pthread_create(&threads[i], NULL, thread_function, &thread_ids[i]); 
+            }
+
+
+
+        for (int i = 0; i < 4; i++) {
+            pthread_join(threads[i], NULL);
+            }
+
+    }
+
+    void threads_function(void *ptr) {
+        pthread_mutex_t lock;
+
+         int thread_id = *(int*)arg;  //taking id of threads
+
+         for (int i = 0; i < a; i++) {
+            while (turn != thread_id); // Busy wait
+            
+            pthread_mutex_lock(&lock);
+
+            if (thread_id==0) b=b+1;
+            if (thread_id==1) b=b+2;
+            if (thread_id==2) b=b+3;
+            if (thread_id==3) b=b+4;
+
+            printf("Thread %d \n", thread_id);
+            turn = (turn + 1) % 4; // Move to the next thread
+            pthread_mutex_unlock(&lock);
+    }
+
+
+    }
+
+
+
+}
+
